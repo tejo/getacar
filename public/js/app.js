55ff412922ec9038c80c42145277284fdd4e4610
Matteo Parmi <m.parmi@gmail.com>
2014-02-02 00:31:26 +0100

    add templates and routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
-var gac = angular.module('getacar', []);
+var gac = angular.module('getacar', ['ngRoute']);
+
+gac.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'home.html',
+        controller: 'HomeController'
+      }).
+      when('/cars/:lat/:lon', {
+        templateUrl: 'cars.html',
+        controller: 'CarsController'
+      }).
+      when('/map/:carId', {
+        templateUrl: 'map.html',
+        controller: 'MapController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }
+]);
+
+gac.factory('DataSharingObject', function(){
+  return {};
+})
 
 gac.factory('carsFactory', function($http, $q){
   return {
@@ -35,50 +60,50 @@ gac.factory('carsFactory', function($http, $q){
   };
 });
 
-
-gac.controller('CarsController', function($scope, carsFactory) {
-  carsFactory.query().then(function(data){
-    $scope.cars = data;
-  });
-
+gac.controller('HomeController', function($scope, $location, $routeParams, carsFactory) {
   $scope.geoCode = function(){
-    carsFactory.geoCode($scope.addrToGeocode).then(function(data){
-      console.log(data)
-      $scope.geo = data
-      $scope.queryByClosest(data)
+    carsFactory.geoCode($scope.addrToGeocode).then(function(geo){
+      console.log(geo)
+      if(!geo.Success){
+        return
+      }
+      $location.path("/cars/"+ geo.Lat +"/"+ geo.Lng)
     });
   }
+});
 
-  $scope.queryByClosest = function(geo){
-    if(!geo.Success){
-      return
-    }
-    carsFactory.query(geo.Lat, geo.Lng).then(function(data){
+gac.controller('MapController', function($scope, $location, $routeParams, carsFactory, DataSharingObject) {
+  $scope.car = DataSharingObject.cars[$routeParams.carId];
+});
+
+gac.controller('CarsController', function($scope, $location, $routeParams, carsFactory, DataSharingObject) {
+  $scope.queryByClosest = function(){
+    $scope.lat = $routeParams.lat;
+    $scope.lon = $routeParams.lon;
+    carsFactory.query($scope.lat, $scope.lon).then(function(data){
       $scope.cars = data;
+      DataSharingObject.cars = data;
     });
-  }
+  }();
 
   $scope.getDistance = function(lat, lon) {
-    if($scope.geo){
-      return (($scope.calculateDistance($scope.geo.Lat, $scope.geo.Lng, lat, lon)*1000).toPrecision(4)).toString() + " mt."
+      return ((calculateDistance($scope.lat, $scope.lon, lat, lon)*1000)
+              .toPrecision(4)).toString() + " mt."
     }
-  }
-
-
-  $scope.calculateDistance = function(lat1,lon1,lat2,lon2) {
-    var R, a, c, d, dLat, dLon, lon1, lon2, sin_dlat_2, sin_dlon_2;
-    R = 6371;
-    dLat = (lat2 - lat1) * Math.PI / 180;
-    dLon = (lon2 - lon1) * Math.PI / 180;
-    lat1 = lat1 * Math.PI / 180;
-    lat2 = lat2 * Math.PI / 180;
-    sin_dlon_2 = Math.sin(dLon / 2);
-    sin_dlat_2 = Math.sin(dLat / 2);
-    a = sin_dlat_2 * sin_dlat_2 + sin_dlon_2 * sin_dlon_2 * Math.cos(lat1) * Math.cos(lat2);
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return d = R * c;
-  }
-
 });
+
+function calculateDistance(lat1,lon1,lat2,lon2) {
+  var R, a, c, d, dLat, dLon, lon1, lon2, sin_dlat_2, sin_dlon_2;
+  R = 6371;
+  dLat = (lat2 - lat1) * Math.PI / 180;
+  dLon = (lon2 - lon1) * Math.PI / 180;
+  lat1 = lat1 * Math.PI / 180;
+  lat2 = lat2 * Math.PI / 180;
+  sin_dlon_2 = Math.sin(dLon / 2);
+  sin_dlat_2 = Math.sin(dLat / 2);
+  a = sin_dlat_2 * sin_dlat_2 + sin_dlon_2 * sin_dlon_2 * Math.cos(lat1) * Math.cos(lat2);
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return d = R * c;
+}
 
 
