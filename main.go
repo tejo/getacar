e1b5aafe1f6b602205ef9e92807165a5eeb6a138
Matteo Parmi <m.parmi@gmail.com>
2014-03-11 23:52:05 +0100

    add absolute path

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"text/template"
 	"time"
 
+	"bitbucket.org/kardianos/osext"
 	"github.com/gorilla/mux"
 	"github.com/tejo/geogo"
 )
@@ -17,20 +18,27 @@ var cars = make([]CarEntry, 0)
 var car2goUrl string = "https://www.car2go.com/api/v2.1/vehicles?loc=milano&oauth_consumer_key=car2gowebsite&format=json"
 var enjoyUrl string = "http://enjoy.eni.com/get_vetture"
 
-var homeTpl *template.Template = template.Must(template.New("index.html").Delims("<%", "%>").Funcs(funcs).ParseFiles("./public/index.html"))
+var homeTpl *template.Template
 var funcs = template.FuncMap{
 	"isIt": isIt,
 }
 
 func isIt(l string) bool { return l == "it" }
 
+func loadTemplate(folderPath string) {
+	homeTpl = template.Must(template.New("index.html").Delims("<%", "%>").Funcs(funcs).ParseFiles(folderPath + "public/index.html"))
+	return
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	homeTpl.Execute(w, map[string]interface{}{"CDNPath":os.Getenv("CDN_PATH")})
+	homeTpl.Execute(w, map[string]interface{}{"CDNPath": os.Getenv("CDN_PATH")})
 }
 
 func main() {
+	folderPath, _ := osext.ExecutableFolder()
+	loadTemplate(folderPath)
 	startClock()
 	fetchCarsFromAPI(car2goUrl, enjoyUrl)
 	r := mux.NewRouter()
@@ -38,7 +46,7 @@ func main() {
 	r.HandleFunc("/geocode", Geocode).Methods("GET")
 	r.HandleFunc("/cars", LoadCars)
 	r.HandleFunc("/cars/{lat}/{lng}", LoadClosestCars).Methods("GET")
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(folderPath + "public/")))
 	http.Handle("/", r)
 
 	port := os.Getenv("PORT")
