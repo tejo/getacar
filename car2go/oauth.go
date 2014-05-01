5a172b590d02d40c2643da6109aaf553c56a1a77
Matteo Parmi <m.parmi@gmail.com>
2014-05-01 09:52:59 +0200

    aggiunti restanti end point

--- car2go/oauth.go
+++ car2go/oauth.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/sessions"
 	"github.com/mrjones/oauth"
@@ -20,11 +21,21 @@ import (
 // var store = sessions.NewCookieStore([]byte("***REMOVED***"), []byte("***REMOVED***"))
 
 var client *Client
+var testMode string
+
+const (
+	sessionName = "car2go-session"
+)
 
 type Client struct {
 	sessions *sessions.CookieStore
 	consumer *oauth.Consumer
 	tokens   map[string]*oauth.RequestToken
+}
+
+type Account struct {
+	AccountID   interface{}
+	Description interface{}
 }
 
 func NewOAuthClient(hashKey []byte, blockKey []byte, consumerKey string, consumerSecret string) *Client {
@@ -48,60 +59,19 @@ func (c *Client) SetDebug(debug bool) {
 	c.consumer.Debug(debug)
 }
 
+func (c *Client) SetTestMode(mode string) {
+	testMode = mode
+}
+
 func init() {
-
-	// tokens = make(map[string]*oauth.RequestToken)
-
-	// var consumerKey *string = flag.String(
-	// 	"consumerkey",
-	// 	"getacar",
-	// 	"Consumer Key from Car2Go. See: https://dev.twitter.com/apps/new")
-
-	// var consumerSecret *string = flag.String(
-	// 	"consumersecret",
-	// 	"***REMOVED***",
-	// 	"Consumer Secret from Car2Go. See: https://dev.twitter.com/apps/new")
-
-	// var port *int = flag.Int(
-	// 	"port",
-	// 	8889,
-	// 	"Port to listen on.")
-
-	// flag.Parse()
-
-	// c = oauth.NewConsumer(
-	// 	*consumerKey,
-	// 	*consumerSecret,
-	// 	oauth.ServiceProvider{
-	// 		RequestTokenUrl:   "https://www.car2go.com/api/reqtoken",
-	// 		AuthorizeTokenUrl: "https://www.car2go.com/api/authorize",
-	// 		AccessTokenUrl:    "https://www.car2go.com/api/accesstoken",
-	// 	},
-	// )
-	// c.Debug(true)
-	// token := &oauth.AccessToken{"fXbl3o0LvQItAHdyUcF6OyPbi", "***REMOVED***", map[string]string{}}
-	// response, err := c.Get(
-	// 	"https://www.car2go.com/api/v2.1/accounts",
-	// 	map[string]string{"loc": "milano", "format": "json"},
-	// 	token)
-	// log.Printf("%#v", response.StatusCode)
-
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-	// defer response.Body.Close()
-
-	// bits, err := ioutil.ReadAll(response.Body)
-	// log.Printf("%#v", string(bits))
-	// return
+	testMode = "1"
 
 	http.HandleFunc("/oauth/car2go/login", RedirectUserToCar2Go)
 	http.HandleFunc("/oauth/car2go", GetCar2GoToken)
-	http.HandleFunc("/oauth/car2go/user", Car2GoUserDetail)
-	// u := fmt.Sprintf(":%d", *port)
-	// fmt.Printf("Listening on '%s'\n", u)
-	// http.ListenAndServe(u, nil)
+	http.HandleFunc("/oauth/car2go/user", Car2GoUserAccount)
+	http.HandleFunc("/oauth/car2go/book", Car2GoCreateBooking)
+	http.HandleFunc("/oauth/car2go/bookings", Car2GoGetBookings)
+	http.HandleFunc("/oauth/car2go/bookings/delete", Car2GoDelateBooking)
 }
 
 func RedirectUserToCar2Go(w http.ResponseWriter, r *http.Request) {
@@ -109,7 +79,6 @@ func RedirectUserToCar2Go(w http.ResponseWriter, r *http.Request) {
 	tokenUrl := fmt.Sprintf("http://%s/maketoken", r.Host)
 	token, requestUrl, err := client.consumer.GetRequestTokenAndUrl(tokenUrl)
 	if err != nil {
-		log.Fatal(err)
 		log.Println(err)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
@@ -119,11 +88,70 @@ func RedirectUserToCar2Go(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, requestUrl, http.StatusTemporaryRedirect)
 }
 
-func Car2GoUserDetail(w http.ResponseWriter, r *http.Request) {
-	session, _ := client.sessions.Get(r, "car2go-session")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+func Car2GoUserAccount(w http.ResponseWriter, r *http.Request) {
+	session, _ := client.sessions.Get(r, sessionName)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(200)
-	fmt.Fprint(w, session.Values["AccountId"], session.Values["Description"])
+	b, _ := json.Marshal(Account{session.Values["AccountId"], session.Values["Description"]})
+	fmt.Fprint(w, string(b))
+}
+
+func Car2GoGetBookings(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	loc := values.Get("loc")
+	accessToken, _ := getAccessTokenAndAccountId(r)
+	response, err := client.consumer.Get(
+		"https://www.car2go.com/api/v2.1/booking",
+		map[string]string{"loc": loc, "format": "json", "test": testMode},
+		accessToken)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	bits, _ := ioutil.ReadAll(response.Body)
+	fmt.Fprint(w, string(bits))
+}
+
+func Car2GoDelateBooking(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	bookingId := values.Get("bookingId")
+	accessToken, _ := getAccessTokenAndAccountId(r)
+	response, err := client.consumer.Delete(
+		"https://www.car2go.com/api/v2.1/booking/"+bookingId,
+		map[string]string{"format": "json", "test": testMode},
+		accessToken)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	bits, _ := ioutil.ReadAll(response.Body)
+	fmt.Fprint(w, string(bits))
+}
+
+func Car2GoCreateBooking(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	vin := values.Get("vin")
+	loc := values.Get("loc")
+	accessToken, accountId := getAccessTokenAndAccountId(r)
+	response, err := client.consumer.Post(
+		"https://www.car2go.com/api/v2.1/bookings",
+		map[string]string{"loc": loc, "format": "json", "vin": vin, "account": accountId, "test": testMode},
+		accessToken)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	bits, _ := ioutil.ReadAll(response.Body)
+	fmt.Fprint(w, string(bits))
 }
 
 func GetCar2GoToken(w http.ResponseWriter, r *http.Request) {
@@ -138,7 +166,7 @@ func GetCar2GoToken(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	session, _ := client.sessions.Get(r, "car2go-session")
+	session, _ := client.sessions.Get(r, sessionName)
 	session.Values["Token"] = accessToken.Token
 	session.Values["Secret"] = accessToken.Secret
 
@@ -149,30 +177,16 @@ func GetCar2GoToken(w http.ResponseWriter, r *http.Request) {
 
 	session.Save(r, w)
 
-	fmt.Fprintf(w, "%°v", accessToken)
-	// response, err := c.Get(
-	// 	"https://www.car2go.com/api/v2.1/accounts",
-	// 	map[string]string{"loc": "milano", "format": "json"},
-	// 	accessToken)
-
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-	// defer response.Body.Close()
-
-	// bits, err := ioutil.ReadAll(response.Body)
-	// fmt.Fprintf(w, string(bits))
-	// log.Printf("%#v", accessToken)
-
-	// {"account":[{"accountId":750314,"description":"Andrea Leverano"}],"returnValue":{"code":0,"description":"Operation successful."}}
+	log.Printf("%#v", accessToken)
+	log.Println(accountId, description)
+	http.Redirect(w, r, "/?login=success", http.StatusTemporaryRedirect)
 }
 
 func GetAccountData(accessToken *oauth.AccessToken) (accountId int, description string) {
 
 	response, err := client.consumer.Get(
 		"https://www.car2go.com/api/v2.1/accounts",
-		map[string]string{"loc": "milano", "format": "json"},
+		map[string]string{"loc": "milano", "format": "json", "test": testMode},
 		accessToken)
 
 	if err != nil {
@@ -201,5 +215,12 @@ func GetAccountData(accessToken *oauth.AccessToken) (accountId int, description 
 	json.Unmarshal(bits, &ad)
 	accountId = ad.Accounts[0].AccountId
 	description = ad.Accounts[0].Description
+	return
+}
+
+func getAccessTokenAndAccountId(r *http.Request) (accessToken *oauth.AccessToken, accountId string) {
+	session, _ := client.sessions.Get(r, sessionName)
+	accountId = strconv.Itoa(session.Values["AccountId"].(int))
+	accessToken = &oauth.AccessToken{session.Values["Token"].(string), session.Values["Secret"].(string), map[string]string{}}
 	return
 }
